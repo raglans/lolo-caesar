e2ffb6542fefe9790aa3e0b0cde438d32e8b110d
nikola@nikola.nikola <nikola@nikola.nikola>
2024-08-21 11:10:34 -0700

    updated styling

--- src/app/page.js
+++ src/app/page.js
@@ -9,15 +9,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={`${styles.center} ${styles.our_approach}`}>
-          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0'}}>Happy 15th Anniversary</h3>
-          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0'}}>to the </h3>
+          <h3 className={`${styles.subtitle} ${dancing_script.className}`} style={{margin: '0', marginTop: '5%'}}>Happy 15th Anniversary</h3>
           <h1 className={styles.title}>LoLo® Caesar</h1>
         <div className={`${styles.center} ${styles.our_approach}`}>
-          <Image style={{marginTop: '15%', height: '55%', width: '55%'}} src={Caesar} />
-          <p style={{margin: '15% 15%'}}>
-            Our signature caesar was created in 2009 under our moniker celebrating local creativity & small business independence.
+          <Image style={{width: '35%', height: '35%'}} src={Caesar} />
+          <p style={{margin: '7% 20% 3% 20%'}}>
+            Our signature Caesar was created in 2009 under our moniker celebrating local creativity & small business independence.
           </p>
-          <a className={styles.button} href="https://www.raglans.ca/">Raglans</a>
+          <p style={{margin: '3% 20% 7% 20%'}}>
+            June 1st, 2024
+          </p>
+          <a className={styles.button} href="https://www.raglans.ca/">Order Now</a>
         </div>
       </div>
     </main>
